c2d9f3b7fffa33a7a580a02c99cdd291fcd62f0f
RajveerSingh711 <rajveer032020@gmail.com>
2025-07-16 11:18:48 +0530

    14 july

---
+++ 15 july/script.js
@@ -0,0 +1,30 @@
+// const crypto = require('crypto');
+
+// const password = 'mySecretPassword';
+// const salt = crypto.randomBytes(16).toString('hex'); // generate a random salt
+// const iterations = 100000;
+// const keyLength = 64; // length of derived key in bytes
+// const digest = 'sha512';
+
+// // Hash the password
+// // For sync
+// const salt1 = crypto.randomBytes(16).toString('hex');
+// const hash1 = crypto.pbkdf2Sync(password, salt1, iterations, keyLength, digest).toString('hex');
+// console.log('Sync Salt:', salt1);
+// console.log('Sync Hash:', hash1);
+
+// // For async
+// const salt2 = crypto.randomBytes(16).toString('hex');
+// crypto.pbkdf2(password, salt2, iterations, keyLength, digest, (err, derivedKey) => {
+//   if (err) throw err;
+//   console.log('Async Salt:', salt2);
+//   console.log('Async Hash:', derivedKey.toString('hex'));
+// });
+
+console.log("ENTRY LEVEL FILE");
+
+var xy=200;
+
+function sumOfTwo(a,b){
+    //250
+}
